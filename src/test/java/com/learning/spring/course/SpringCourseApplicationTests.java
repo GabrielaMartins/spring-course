8f52f6b00aaf0f98cdd7da026d207728d6e4fb48
Gabriela Martins <gabrielajsmartins@gmail.com>
2022-05-31 15:54:15 +0100

    Journaler API: First commit

---
+++ src/test/java/com/learning/spring/course/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
